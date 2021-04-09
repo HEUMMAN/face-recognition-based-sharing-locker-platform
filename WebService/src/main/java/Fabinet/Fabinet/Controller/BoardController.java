d72cac9c5c11d6cac0f45d61c63f7bd072fd8be4
llshl <lsh970708@naver.com>
2021-04-10 07:32:55 +0900

    Add showOneBoard method
    게시글 1개 불러오기 메서드 추가
    URI경로를 통한 자원 호출 방식으로 수정

--- WebService/src/main/java/Fabinet/Fabinet/Controller/BoardController.java
+++ WebService/src/main/java/Fabinet/Fabinet/Controller/BoardController.java
@@ -3,18 +3,20 @@ package Fabinet.Fabinet.Controller;
 import Fabinet.Fabinet.DTO.BoardDTO;
 import Fabinet.Fabinet.Domain.Board;
 import Fabinet.Fabinet.Domain.Member;
+import Fabinet.Fabinet.Exception.BoardNotFoundException;
 import Fabinet.Fabinet.Service.BoardService;
 import Fabinet.Fabinet.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -28,17 +30,28 @@ public class BoardController {
     private final MemberService memberService;
 
     //게시글 불러오기
-    @ResponseBody
-    @GetMapping("/list")
+    @GetMapping("/post")
     public List showEntireBoard(HttpServletResponse response){
         log.info("게시글 불러오기");
         List<Board> boards = boardService.findBoards();
         return boards;
     }
 
+    //게시글 1개 불러오기
+    @GetMapping("/{id}")
+    public Board showOneBoard(@PathVariable int id, HttpServletResponse response){
+        log.info(id+"번 게시글 불러오기");
+        Board board = boardService.findOne(id);
+        //존재하지 않는 게시물 조회시 예외 상태코드 반환
+        if (board == null) {
+            throw new BoardNotFoundException(String.format("ID[%s] not found",id));
+        }
+        return board;
+    }
+
     //게시글 작성 DB등록
     @PostMapping("/post")
-    public void createBoard(@RequestBody BoardDTO boardDTO, HttpServletRequest request) throws IOException {
+    public ResponseEntity<BoardDTO> createBoard(@RequestBody BoardDTO boardDTO, HttpServletRequest request) throws IOException {
 
         log.info("새로운 게시글 DB등록 시작");
         HttpSession session = request.getSession();
@@ -46,24 +59,35 @@ public class BoardController {
         log.info("유저ID: "+sessionId);
         LocalDateTime now = LocalDateTime.now();    //날짜+시간 형식
 
-        Member member = memberService.findOne(sessionId);
-        System.out.println("Member name: "+member.getName());
-        System.out.println("Member loginID: "+member.getLoginId());
-
         Board board = new Board();
+        if(sessionId!=null){
+            Member member = memberService.findOne(sessionId);
+            System.out.println("Member name: "+member.getName());
+            System.out.println("Member loginID: "+member.getLoginId());
+            board.setAuthor(sessionId);
+            board.setMember(member);
+        }
+        else{
+            board.setAuthor("guest");
+            board.setMember(null);
+        }
         board.setTitle(boardDTO.getTitle());
         board.setContent(boardDTO.getContent());
-        board.setAuthor(sessionId);
+
         board.setDate(now);
-        board.setMember(member);
 
         System.out.println("제목: "+boardDTO.getTitle());
         System.out.println("내용: "+boardDTO.getContent());
         System.out.println("시간: "+now);
         System.out.println("작성자: "+sessionId);
-        System.out.println("멤버객체: "+member);
 
         String result = boardService.createBoard(board);
         log.info("["+result+"]에 대한 게시글 등록 완료");
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(board.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
