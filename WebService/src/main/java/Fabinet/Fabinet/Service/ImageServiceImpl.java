71cc969e6c21be6e1714b72ff94eaf81e74ec5b4
llshl <52540882+llshl@users.noreply.github.com>
2021-05-03 02:28:45 +0900

    Update ImageServiceImpl.java

--- WebService/src/main/java/Fabinet/Fabinet/Service/ImageServiceImpl.java
+++ WebService/src/main/java/Fabinet/Fabinet/Service/ImageServiceImpl.java
@@ -12,13 +12,13 @@ import java.sql.Blob;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true) //트랜섹셔널해줘야함
+@Transactional(readOnly = true) 
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService{
 
     private final ImageRepository imageRepository;
 
-    @Transactional  //트랜섹셔널해줘야함
+    @Transactional  
     public String join(Image image){
         imageRepository.save(image);
         return "complete";
