587c73ff92a6e3bc24363a7f2dd1fc01300139e7
llshl <lsh970708@naver.com>
2021-05-17 22:51:29 +0900

    Change to gradle

--- WebService/src/main/java/Jongsul/fabinetmaven/Mqtt/MqttPubThreadRun.java
+++
@@ -1,8 +0,0 @@
-package Jongsul.fabinetmaven.Mqtt;
-
-public class MqttPubThreadRun extends Thread{
-    public void run(){
-        MqttPublishUserID mqttPublishUserID = new MqttPublishUserID();
-        mqttPublishUserID.MqttPub();
-    }
-}
--- WebService/src/main/java/Jongsul/fabinetmaven/Mqtt/MqttSubThreadRun.java
+++
@@ -1,8 +0,0 @@
-package Jongsul.fabinetmaven.Mqtt;
-
-public class MqttSubThreadRun extends Thread{
-    public void run(){
-        MqttSubscribeUserID mqttSubscribeUserID = new MqttSubscribeUserID();
-        mqttSubscribeUserID.MqttSub();
-    }
-}
