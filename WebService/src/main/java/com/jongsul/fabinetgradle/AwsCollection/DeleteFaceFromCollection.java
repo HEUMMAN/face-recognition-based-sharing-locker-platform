5a7d70ff80a9eda9469afc55603c5a86b3d46d31
llshl <lsh970708@naver.com>
2021-07-07 03:26:21 +0900

    aws collection delete 메서드 추가

--- WebService/src/main/java/com/jongsul/fabinetgradle/AwsCollection/DeleteFaceFromCollection.java
+++ WebService/src/main/java/com/jongsul/fabinetgradle/AwsCollection/DeleteFaceFromCollection.java
@@ -13,10 +13,10 @@ import java.util.List;
 public class DeleteFaceFromCollection {
     public static final String collectionId = "Collection";
     //컬랙션에서 지울 사진의 id
-    public static final String faces[] = {"e64b05a5-29f2-446a-8d12-92fc50488ce9"
-    ,"e431e60b-2da1-434a-9555-d7d3678a3519"
-    ,"b18894b6-5b84-43b9-b6d3-4db0c0b63d1d"
-    ,"64412b02-f159-4432-9186-412ee2f8e468"};
+    public static final String faces[] = {"06e1c661-eb36-46e6-bc16-ce8d50d095f6"
+    ,"8e019913-84a5-451a-96c7-0b0ca9d0be31"
+    ,"d5d31ab2-fe33-4295-af8f-943fe4619f92"
+    ,"dd73532f-ab09-4c57-b785-0372899f13c0"};
 
     public static void main(String[] args) throws Exception {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIA4EPX72XC3ACFOWVU", "BnW0X9nSPsqJN07KXgjKreizEa4Q7BI4I9Qdrytd");
